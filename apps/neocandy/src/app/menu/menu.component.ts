399d864baabb322c9b4f7b6a0a0c43f98d367065
ykoitzsch <y.koitzsch@gmx.at>
2022-02-07 16:26:06 +0100

    update logo, colors, links, descriptions

--- apps/neocandy/src/app/menu/menu.component.ts
+++ apps/neocandy/src/app/menu/menu.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 
+const BOUNTY_GOOGLE_DOC =
+  'https://docs.google.com/document/d/1Qo3-Gdk7uewiTwczt716KGMnQb2ByDVTKKt5xq_pGss/edit?usp=sharing';
+const LITE_PAPER =
+  'https://docs.google.com/document/d/1aRYe4de3PKGYtCW8jQSzU51jDjO_X1bYW3BhgoVLChs/edit?usp=sharing';
+
 @Component({
   selector: 'nc-menu',
   templateUrl: './menu.component.html',
@@ -19,12 +24,20 @@ export class MenuComponent implements OnInit {
         command: () => this.router.navigate([''], { fragment: 'features' }),
       },
       {
-        label: 'Get Candy',
+        label: 'Get $CANDY',
         command: () => this.router.navigate([''], { fragment: 'free-candy' }),
       },
       {
         label: 'Games',
         command: () => this.router.navigate([''], { fragment: 'games' }),
+      },
+      {
+        label: 'Bounty Program',
+        command: () => window.open(BOUNTY_GOOGLE_DOC, '_blank'),
+      },
+      {
+        label: 'Lite Paper',
+        command: () => window.open(LITE_PAPER, '_blank'),
       },
       {
         label: 'FAQ',
