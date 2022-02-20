a63b584a8b78787cf2e6c7d321cf76aad79f36ff
ykoitzsch <y.koitzsch@gmx.at>
2022-02-20 23:10:08 +0100

    create prototype landing page for candyclash

--- apps/candyclash/src/app/app.module.ts
+++ apps/candyclash/src/app/app.module.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, CommonModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
