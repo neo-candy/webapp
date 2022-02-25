cf7d9efb69124bda83c56bb7d0b2e970877d77d0
ykoitzsch <y.koitzsch@gmx.at>
2022-02-25 21:37:55 +0100

    fix loading issue

--- apps/candyclash/src/app/app.component.ts
+++ apps/candyclash/src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
 
+interface NFT {
+  image: string;
+  type: string;
+  staked: boolean;
+  sugar: string;
+  multiplier: string;
+}
 @Component({
   selector: 'cc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'candyclash';
+export class AppComponent implements OnInit {
+  public items: MenuItem[] = [];
+  public nfts: NFT[] = [
+    {
+      image:
+        'https://gateway.pinata.cloud/ipfs/Qmeqst3PBH9CQUxmZrcoT45HeGFVd89aUYrHN6vwFuhTDJ/0.png',
+      multiplier: '120%',
+      staked: true,
+      sugar: '5',
+      type: 'Villain',
+    },
+    {
+      image:
+        'https://gateway.pinata.cloud/ipfs/Qmeqst3PBH9CQUxmZrcoT45HeGFVd89aUYrHN6vwFuhTDJ/0.png',
+      multiplier: '120%',
+      staked: true,
+      sugar: '5',
+      type: 'Villain',
+    },
+    {
+      image:
+        'https://gateway.pinata.cloud/ipfs/Qmeqst3PBH9CQUxmZrcoT45HeGFVd89aUYrHN6vwFuhTDJ/0.png',
+      multiplier: '120%',
+      staked: true,
+      sugar: '5',
+      type: 'Villain',
+    },
+    {
+      image:
+        'https://gateway.pinata.cloud/ipfs/Qmeqst3PBH9CQUxmZrcoT45HeGFVd89aUYrHN6vwFuhTDJ/0.png',
+      multiplier: '120%',
+      staked: true,
+      sugar: '5',
+      type: 'Villain',
+    },
+    {
+      image:
+        'https://gateway.pinata.cloud/ipfs/Qmeqst3PBH9CQUxmZrcoT45HeGFVd89aUYrHN6vwFuhTDJ/0.png',
+      multiplier: '120%',
+      staked: true,
+      sugar: '5',
+      type: 'Villain',
+    },
+    {
+      image:
+        'https://gateway.pinata.cloud/ipfs/Qmeqst3PBH9CQUxmZrcoT45HeGFVd89aUYrHN6vwFuhTDJ/0.png',
+      multiplier: '120%',
+      staked: true,
+      sugar: '5',
+      type: 'Villain',
+    },
+    {
+      image:
+        'https://gateway.pinata.cloud/ipfs/Qmeqst3PBH9CQUxmZrcoT45HeGFVd89aUYrHN6vwFuhTDJ/0.png',
+      multiplier: '120%',
+      staked: true,
+      sugar: '5',
+      type: 'Villain',
+    },
+  ];
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+
+    this.items = [
+      {
+        label: 'Stake',
+        icon: 'pi pi-play',
+        disabled: true,
+      },
+      {
+        label: 'Unstake',
+        icon: 'pi pi-pause',
+        disabled: true,
+      },
+    ];
+  }
 }
--- apps/candyclash/src/app/app.module.ts
+++ apps/candyclash/src/app/app.module.ts
@@ -3,12 +3,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { TabViewModule } from 'primeng/tabview';
+import { RippleModule } from 'primeng/ripple';
+import { TableModule } from 'primeng/table';
+import { MenuModule } from 'primeng/menu';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CommonModule, ButtonModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    ButtonModule,
+    MenubarModule,
+    TabViewModule, //required to use ng-template in primeng components,
+    RippleModule,
+    TableModule,
+    MenuModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
