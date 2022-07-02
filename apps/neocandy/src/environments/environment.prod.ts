4f40601bd22c1ff6de8a1db60d10b27a960512b2
ykoitzsch <y.koitzsch@gmx.at>
2022-07-02 13:21:25 +0200

    update env

--- apps/neocandy/src/environments/environment.prod.ts
+++ apps/neocandy/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   testnet: {
     neocandy: '0xa90c11b6f71cee327bf9f62060641e5ad22f57e6',
-    lollipopNFT: '0x1a3577a71d69d5236b2ffc3ba1e333db2619fb7e',
-    nodeUrl: 'https://neo3-testnet.neoline.vip:443',
+    lollipopNFT: '0xcdbb7bd33c623510f94626d1953f95a59c27645c',
+    nodeUrl: 'http://seed1.ngd.network:10332',
   },
 };
